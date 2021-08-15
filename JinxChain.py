30ae96239f284818ec5588223fac4fbd4e1c2fa2
VisualTaggy <visualtaggy@gmail.com>
2021-08-15 14:27:16 +0530

    + function to check integrity of block chain

--- JinxChain.py
+++ JinxChain.py
@@ -13,28 +13,38 @@ def update_hash(*args):
 
 
 class Block():
-    tran = None
-    hash = None
-    nonce = 0
-    previous_hash = "0" * 64
 
-    def __init__(self, tran, number=0):
+    def __init__(self, number=0, previous_hash="0"*64, tran=None, nonce=0):
         self.tran = tran
         self.number = number
+        self.previous_hash = previous_hash
+        self.nonce = nonce
 
     def hash(self):
-        return update_hash(self.previous_hash, self.number, self.tran, self.nonce)
+        return update_hash(
+            self.number,
+            self.previous_hash,
+            self.tran,
+            self.nonce
+        )
 
     def __str__(self):
-        return str("Block#: %s\nHash: %s\nPreviousHash: %s\nData: %s\nNonce: %s\n" % (self.number, self.hash(), self.previous_hash, self.tran, self.nonce))
+        return str("Block: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" % (
+            self.number,
+            self.hash(),
+            self.previous_hash,
+            self.tran,
+            self.nonce
+        )
+        )
 
 
 class JinxChain():
     # should be dynamic and increase as the number of clients / users on the blockchain increase!
     difficulty = 3
 
-    def __init__(self, chain=[]):
-        self.chain = chain
+    def __init__(self):
+        self.chain = []
 
     def add_block(self, block):
         self.chain.append(block)
@@ -55,19 +65,36 @@ class JinxChain():
             else:
                 block.nonce += 1
 
+    def verify_integrity(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i-1].hash()
+            if _previous != _current or _current[:self.difficulty] != "0"*self.difficulty:
+                return False
+
+        return True
+
 
 def testCode():
     blockchain = JinxChain()
+    database = ["sending money", "receiving money",
+                "receiving money", "sending money"]
+    num = 0
 
-    trans = ['sending money', 'receiving money', 'receiving money']
-
-    idx = 0
-    for tran in trans:
-        idx += 1
-        blockchain.mine_coin(Block(tran, idx))
+    for tran in database:
+        num += 1
+        blockchain.mine_coin(Block(num, tran=tran))
 
     for block in blockchain.chain:
         print(block)
+
+    print(blockchain.verify_integrity())
+
+    blockchain.chain[2].tran = "HACKED MONEY"
+    for block in blockchain.chain:
+        print(block)
+    # blockchain.mine_coin(blockchain.chain[2])
+    print(blockchain.verify_integrity())
 
 
 if __name__ == '__main__':
