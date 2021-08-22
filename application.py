20ef5daeaa56b057ceaf36ff7a0ba71cc1de243c
VisualTaggy <visualtaggy@gmail.com>
2021-08-22 21:01:44 +0530

    added logout code to driver code

--- application.py
+++ application.py
@@ -74,6 +74,13 @@ def login():
 
     return render_template('login.html')
 
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("Logout success","success")
+    return redirect(url_for('login'))
+
 @app.route("/dashboard")
 def dashboard():
     return render_template('dashboard.html',session=session)
